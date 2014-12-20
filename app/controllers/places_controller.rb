80886adf54b036f685a6a69f1371f1ad1c227764
Jason Joy <jasondjoy@gmail.com>
2014-12-20 19:36:53 +0000

    Restrict access to edit places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-	before_action :authenticate_user!, :only => [:new, :create]
+	before_action :authenticate_user!, :only => [:new, :create, :edit, :update]
 
 	def index
 		@places = Place.all.page(params[:page]).per(5)
@@ -20,6 +20,10 @@ class PlacesController < ApplicationController
 
 	def edit
 		@place = Place.find(params[:id])
+
+		if @place.user != current_user
+			return render :text => 'Not Allowed', :status => :forbidden
+		end
 	end
 
 	def update
